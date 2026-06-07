075aab668af8081eb5e02a596f7b7504e8d81391
bobronsernio-boop <bobronsernio@gmail.com>
2026-06-07 10:25:51 +0100

    Use environment variable for server port

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ import { handleProxy, generateSW } from "./src/proxyEngine.js";
 import youtubesearchapi from 'youtube-search-api';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(cookieParser());
