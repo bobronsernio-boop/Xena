b6d2c5556c613c207cb2f0d9bf7ef41a8392f110
bobronsernio-boop <bobronsernio@gmail.com>
2026-06-07 14:23:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       '/fetch': 'http://localhost:3000',
-      '/proxy': 'http://localhost:3000',
-      '/gateway': 'http://localhost:3000',
+      '/sw': 'http://localhost:3000',
+      '/rev': 'http://localhost:3000',
+      '/bin': 'http://localhost:3000',
       '/api': 'http://localhost:3000',
-      '/view': 'http://localhost:3000',
-      '/sw.js': 'http://localhost:3000',
-    }
-  }
-});
+      '/view': 'http://localhost:3000
